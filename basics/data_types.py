2d76f20ee8b1ff86f13b6eee0448db3a88bcd371
bmanojna <bmanojna2007@gmail.com>
2026-03-12 17:09:52 +0530

    Add files via upload

---
+++ basics/data_types.py
@@ -0,0 +1,18 @@
+
+
+#  data types
+from math import sqrt
+import math
+
+
+a = 1
+print(type(a))
+b = 0.009
+print(type(b))
+m="nano"
+print(type(m))
+
+c = math.hypot(4, 3)
+print(type(c))
+d = math.sqrt(16)
+print(type(d))
---
+++ basics/variables.py
@@ -0,0 +1,10 @@
+# variable assignment
+x=0
+y=9
+print(x+y)
+# variable assignment using different data types
+name = "manuu"
+print(name)
+# 
+i = {}
+print("the type of i is ",type(i))
